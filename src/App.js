d2719fb8f988bcce5c4af7417fcb64fb31246410
unknown <coder.null@gmail.com>
2019-08-26 23:40:05 -0700

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import * as firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+
+  constructor() {
+    super();
+
+    this.state = {
+      messages: [],
+      author: '',
+      message: ''
+    };
+  }
+
+  componentDidMount() {
+    const db = firebase.firestore();
+
+    db.collection('messages').onSnapshot(querySnapshot => {
+      const messages = [];
+      querySnapshot.forEach(doc => {
+        const message = doc.data();
+        message.id = doc.id;
+        messages.push(message);
+      });
+
+      this.setState({
+        messages
+      });
+    });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const db = firebase.firestore();
+    db.collection('messages').add({
+      author: this.state.author,
+      message: this.state.message,
+      dateSent: new Date()
+    });
+
+    this.setState({
+      author: '',
+      message: ''
+    });
+  };
+
+  handleAuthorChange = e => {
+    this.setState({
+      author: e.target.value
+    });
+  };
+
+  handleMessageChange = e => {
+    this.setState({
+      message: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input
+            type="text"
+            value={this.state.author}
+            onChange={this.handleAuthorChange}
+            placeholder="name"
+          />
+          <input
+            type="text"
+            value={this.state.message}
+            onChange={this.handleMessageChange}
+            placeholder="message"
+          />
+          <button onClick={this.handleSubmit}>Submit</button>
+        </form>
+
+        <div>
+          {this.state.messages.map(message => (
+            <div key={message.id} className="message">
+              <span>{message.author}</span>
+              &nbsp;-&nbsp;
+              <span>{message.message}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+
+const config = {
+    apiKey: "AIzaSyCP1wlIxiqFnbfE0OevMxqmgk1v6mzaViA",
+    authDomain: "react-chat-76f89.firebaseapp.com",
+    databaseURL: "https://react-chat-76f89.firebaseio.com",
+    projectId: "react-chat-76f89",
+    storageBucket: "react-chat-76f89.appspot.com",
+    messagingSenderId: "483575731254",
+    appId: "1:483575731254:web:5eaf0176da238c9b"
+  };
+
+  firebase.initializeApp(config);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
