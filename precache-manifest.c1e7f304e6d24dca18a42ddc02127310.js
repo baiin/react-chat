3120db1cbcd7b0d912d31d5dd3aa3c967909b8cf
baiin <ching.lynard@gmail.com>
2019-08-29 05:01:46 -0700

    Updates

---
+++ precache-manifest.c1e7f304e6d24dca18a42ddc02127310.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d402a3ff59b3cda819b9398bd55b599",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "c6f27fc3b3e7e6152a20",
+    "url": "/react-chat/static/css/main.bc34ba4d.chunk.css"
+  },
+  {
+    "revision": "b71adb4b0dfd575ef975",
+    "url": "/react-chat/static/js/2.85907ea2.chunk.js"
+  },
+  {
+    "revision": "c6f27fc3b3e7e6152a20",
+    "url": "/react-chat/static/js/main.d26bdd4b.chunk.js"
+  },
+  {
+    "revision": "a0eceef403aea6bf6d83",
+    "url": "/react-chat/static/js/runtime~main.c7a137d1.js"
+  }
+]);
