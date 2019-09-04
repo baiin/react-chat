53afce92f7b4b2fb9c164a2791519fcfb261b40f
unknown <coder.null@gmail.com>
2019-09-04 02:21:37 -0700

    added configs

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import * as firebase from "firebase";
-import * as CONFIG from "./config";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
 
-const config = {
-  apiKey: CONFIG.APIKEY,
-  authDomain: CONFIG.AUTHDOMAIN,
-  databaseURL: CONFIG.DATABASEURL,
-  projectId: CONFIG.PROJECTID,
-  storageBucket: CONFIG.STORAGEBUCKET,
-  messagingSenderId: CONFIG.MESSAGINGSENDERID,
-  appId: CONFIG.APPID
+const firebaseConfig = {
+  apiKey: 'AIzaSyCP1wlIxiqFnbfE0OevMxqmgk1v6mzaViA',
+  authDomain: 'react-chat-76f89.firebaseapp.com',
+  databaseURL: 'https://react-chat-76f89.firebaseio.com',
+  projectId: 'react-chat-76f89',
+  storageBucket: 'react-chat-76f89.appspot.com',
+  messagingSenderId: '483575731254',
+  appId: '1:483575731254:web:5eaf0176da238c9b'
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
